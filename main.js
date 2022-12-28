a383de78fef679e5597110b1aa1798baa96535ed
Luiz Henrique Dutra <muild123@hotmail.com>
2022-12-28 05:14:22 -0300

    adicionado função parseInt

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 const formulario = document.getElementById('form-js');
   formulario.addEventListener('submit', function(event) {
-    event.preventDefault();
+    
 
-    const valor1 = document.getElementById('valor1').value;
-    const valor2 = document.getElementById('valor2').value;
+    const valor1 = parseInt(document.getElementById('valor1').value);
+    const valor2 = parseInt(document.getElementById('valor2').value);
 
     if (valor2 > valor1) {
       alert('O segundo valor é maior que o primeiro, está correto.');
@@ -11,4 +11,5 @@ const formulario = document.getElementById('form-js');
     } else {
       alert('O primeiro valor é maior ou igual o segundo, está incorreto');
     }
+    event.preventDefault();
 })
